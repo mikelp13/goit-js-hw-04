3f3c4fa4db46a13a63ecbee9eed820b4efabdb1a
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-11-16 15:13:24 +0200

    added solved task-02

--- task-02.js
+++ task-02.js
@@ -10,36 +10,30 @@ const inventory = {
   },
 };
 
-const invokeInventoryAction = function(itemName, action) {
-  const act =  action(itemName);
-  const msg =  `Invoking action on ${itemName}`;
-  return {act, msg};
+const invokeInventoryAction = function (itemName, action) {
+  const act = action(itemName);
+  const msg = `Invoking action on ${itemName}`;
+  return { act, msg };
 };
 
-const invokeAdd = invokeInventoryAction(
-  'Medkit',
-  inventory.add  // Write code in this line
-);
+const invokeAdd = invokeInventoryAction('Medkit', inventory.add.bind(inventory));
 const arrayAdd = [...inventory.items];
-/* 
-//console.log(invokeAdd);
+ 
+console.log(invokeAdd);
 //{ act: 'Adding Medkit to inventory', msg: 'Invoking action on Medkit' }
 
-//console.log(arrayAdd);
+console.log(arrayAdd);
 // ['Knife', 'Gas mask', 'Medkit']
-*/
 
-const invokeRemove = invokeInventoryAction(
-  'Gas mask',
-  inventory.remove  // Write code in this line
-);
 
-const arrayRemove = [...inventory.items];
+ const invokeRemove = invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
 
-/* 
-//console.log(invokeRemove);
+ const arrayRemove = [...inventory.items];
+
+
+console.log(invokeRemove);
 //{ act: 'Removing Gas mask from inventory', msg: 'Invoking action on Gas mask' }
 
-//console.log(arrayRemove);
+console.log(arrayRemove);
 // ['Knife', 'Medkit']
-*/
+
